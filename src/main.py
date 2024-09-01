bdc586b46b8857b9ba58944e6e7fc06d47c46850
Vadim Pchelintsev <vadim.1.95@mail.ru>
2024-09-01 21:02:20 +0300

    1. добавил middleware для exceptions 2. добавил handler для exceptions 3. зарегистрировал маршруты для приложения auth

--- src/main.py
+++ src/main.py
@@ -3,11 +3,17 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi.exceptions import RequestValidationError
+
+# from auth import auth_router
 
 from config import settings
 from database import db_helper
+from schemas import ErrorResponse
 
-# from api import router as api_router
+from middleware import ExceptionMiddleware
+from exceptions import validation_exception_handler, CustomHTTPException, custom_http_exception_handler
+from auth import router as auth_router
 
 
 @asynccontextmanager
@@ -22,8 +28,20 @@ async def lifespan(app: FastAPI):
 main_app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
+    root_path="/api/v1",
+    responses={
+        400: {"model": ErrorResponse, "description": "Bad Request"},
+        422: {"model": ErrorResponse, "description": "Validation Error"},
+        500: {"model": ErrorResponse, "description": "Internal Server Error"},
+    },
 )
-# main_app.include_router(api_router)
+
+main_app.add_middleware(ExceptionMiddleware)
+main_app.add_exception_handler(RequestValidationError, validation_exception_handler)
+main_app.add_exception_handler(CustomHTTPException, custom_http_exception_handler)
+
+main_app.include_router(auth_router)
+# main_app.include_router(user_router)
 
 if __name__ == "__main__":
     uvicorn.run(
