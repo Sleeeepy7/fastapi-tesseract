926553da52970e0c5212c589dd862641ecd1bf4f
Vadim Pchelintsev <vadim.1.95@mail.ru>
2024-09-01 21:00:35 +0300

    тест модель User

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,9 +1,12 @@
+import secrets
 from typing import TYPE_CHECKING
 
-from src.models import Base
+from models import Base
+from mixins import PrimaryKeyMixin, TimeStampMixin
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, ForeignKey
+from sqlalchemy import Integer, ForeignKey, String, LargeBinary, Boolean, event
+
 
 # from fastapi_users_db_sqlalchemy import (
 #     SQLAlchemyBaseUserTable,
@@ -18,20 +21,23 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base):
-    pass
-    # @classmethod
-    # def get_db(cls, session: "AsyncSession"):
-    #     return SQLAlchemyUserDatabase(session, User)
+class User(Base, PrimaryKeyMixin, TimeStampMixin):
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    token: Mapped[str | None] = mapped_column(String, nullable=True)
 
+    @staticmethod
+    def generate_token() -> str:
+        return secrets.token_hex(32)  # генерация токена
 
-# class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[int]):
-#     user_id: Mapped[int] = mapped_column(
-#         Integer,
-#         ForeignKey("users.id", ondelete="CASCADE"),
-#         nullable=False,
-#     )
+    @classmethod
+    def __declare_last__(cls):
+        super().__declare_last__()
+        event.listen(cls, "before_insert", cls._generate_token)
 
-#     @classmethod
-#     def get_db(cls, session: "AsyncSession"):
-#         return SQLAlchemyAccessTokenDatabase(session, AccessToken)
+    @staticmethod
+    def _generate_token(mapper, connection, target):
+        if not target.token:
+            target.token = User.generate_token()
