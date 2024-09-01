8803829c3e4bb18d84a5d82111846188d5066865
Vadim Pchelintsev <vadim.1.95@mail.ru>
2024-09-01 20:58:40 +0300

    Добавил: 1. PrimaryKeyMixin 2. TimeStampMixin

--- src/mixins.py
+++ src/mixins.py
@@ -1,5 +1,28 @@
-# from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import event, func, DateTime
+from datetime import datetime
 
 
-# class IntIdPkMixin:
-#     id: Mapped[int] = mapped_column(primary_key=True)
+class PrimaryKeyMixin:
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class TimeStampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        server_default=func.now(),
+        default=datetime.utcnow,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        server_default=func.now(),
+        default=datetime.utcnow,
+    )
+
+    @staticmethod
+    def _updated_at(mapper, connection, target):
+        target.updated_at = datetime.utcnow()
+
+    @classmethod
+    def __declare_last__(cls):
+        event.listen(cls, "before_update", cls._updated_at)
