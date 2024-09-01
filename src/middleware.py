d4f0bf246ad33ff7d482e0ae00fedb37cbdb4e49
Vadim Pchelintsev <vadim.1.95@mail.ru>
2024-09-01 20:57:58 +0300

    добавил прослойку ExceptionMiddleware для типизированного ответа ошибочных сообщений

---
+++ src/middleware.py
@@ -0,0 +1,45 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi import Request, status
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+from fastapi.exceptions import RequestValidationError
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class ExceptionMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            response = await call_next(request)
+        except RequestValidationError as e:  # Обработка валидационных ошибок
+            log.exception(e)
+            response = JSONResponse(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                content={
+                    "status": False,
+                    "error": "Validation error",
+                    "details": e.errors(),
+                },
+            )
+        except ValidationError as e:  # Обработка ошибок валидации Pydantic
+            log.exception(e)
+            response = JSONResponse(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                content={
+                    "status": False,
+                    "error": "Validation error",
+                    "details": e.errors(),
+                },
+            )
+        except Exception as e:  # Обработка всех других ошибок
+            log.exception(e)
+            response = JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                    "status": False,
+                    "error": "An unexpected error occurred.",
+                    "details": e.errors(),
+                },
+            )
+        return response
