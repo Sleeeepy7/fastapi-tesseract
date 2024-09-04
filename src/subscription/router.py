b9deb0db3da4948e12d61a975306c9472c1d2a0a
Vadim Pchelintsev <vadim.1.95@mail.ru>
2024-09-04 13:49:18 +0300

    значительные изменения
    1. /create - создание плана подписки (только для админов)
    2. /get_sub_plans - получение всех активных планов
    3. /test_router - проверка current_user через токен

--- src/subscription/router.py
+++ src/subscription/router.py
@@ -1,6 +1,18 @@
-from fastapi import APIRouter, Depends
+from typing import List
 
-from src.auth.permissions import PermissionsDependency, IsAuthenticatedPermission
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.auth.permissions import PermissionsDependency, IsAuthenticatedPermission, IsAdminPermission
+from src.database import db_helper
+from src.auth.models import User
+from src.exceptions import CustomHTTPException
+from src.schemas import BaseResponse
+from src.auth.dependencies import get_current_user
+
+from .schemas import SubscriptionPlanResponse, SubscriptionPlanCreate, SubscriptionPlanRead
+from .service import create_subscription_plan, get_subscription_plan_list
 
 
 subscription_router = APIRouter()
@@ -22,8 +34,46 @@ subscription_router = APIRouter()
 #     return {"test"}
 
 
-@subscription_router.get(
-    "",
+@subscription_router.post(
+    "/create",
+    dependencies=[
+        Depends(
+            PermissionsDependency(
+                [
+                    IsAuthenticatedPermission,
+                    IsAdminPermission,
+                ]
+            )
+        )
+    ],
+    response_model=SubscriptionPlanResponse,
 )
-async def test():
-    return {"test"}
+async def create_sub_plan(
+    sub_plan_in: SubscriptionPlanCreate,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    sub_plan = await create_subscription_plan(session, sub_plan_in)
+    return SubscriptionPlanResponse(data=sub_plan)
+
+
+@subscription_router.get("", response_model=BaseResponse[list[SubscriptionPlanRead]])
+async def get_sub_plans(
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    sub_plans = await get_subscription_plan_list(session)
+    if not sub_plans:
+        raise CustomHTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Subscription plans are not available yet.",
+        )
+    return BaseResponse(data=sub_plans)
+
+
+@subscription_router.get("/me")
+async def test_router(
+    session: AsyncSession = Depends(db_helper.session_getter),
+    current_user: User = Depends(get_current_user),
+):
+    print("################################")
+    print(current_user)
+    print(current_user.email)
