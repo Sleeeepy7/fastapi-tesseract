35707e6cad4dbdea2a4a79a885d6d658bbfb0c92
Vadim Pchelintsev <vadim.1.95@mail.ru>
2024-09-09 21:16:12 +0300

    добавил новые функции
    1.validate_subscription_purchase
    2. buy_subscription_plan
    3. get_user_subscription_plan

--- src/subscription/service.py
+++ src/subscription/service.py
@@ -1,8 +1,10 @@
 from typing import Optional, List
+from datetime import datetime, timedelta
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import selectinload
 
 from .schemas import SubscriptionPlanCreate, SubscriptionPlanRead
 from .models import SubscriptionPlan, UserSubscription
@@ -30,10 +32,50 @@ async def get_subscription_plan_by_id(session: AsyncSession, subscription_id: in
     return sub_plan
 
 
-async def buy_subscription_plan(session: AsyncSession, user: User, subscription_plan_id: int) -> SubscriptionPlan:
-    subscription_plan = await get_subscription_plan_by_id(session, subscription_plan_id)
+async def validate_subscription_purchase(session: AsyncSession, user: User, payment_data: int):
+    subscription_plan = await get_subscription_plan_by_id(session, payment_data.plan_id)
     if not subscription_plan or not subscription_plan.is_active:
-        pass
-        # raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Subscription plan not found or inactive.")
+        raise ValueError("Subscription plan not found or inactive.")
 
-    pass
+    if not user:
+        raise ValueError("User not found.")
+
+    if user.balance < subscription_plan.price:
+        raise ValueError("Insufficient funds to purchase a subscription.")
+
+    await session.refresh(user, attribute_names=["active_subscription"])
+
+    if user.active_subscription and user.active_subscription.is_active():
+        raise ValueError("The user already has an active subscription.")
+
+    return subscription_plan
+
+
+async def buy_subscription_plan(session: AsyncSession, user: User, payment_data: int) -> SubscriptionPlan:
+    subscription_plan = await validate_subscription_purchase(session, user, payment_data)
+
+    user.balance -= subscription_plan.price
+
+    start_date = datetime.utcnow()
+    end_date = start_date + timedelta(days=subscription_plan.duration_days)
+
+    new_subscription = UserSubscription(
+        user_id=user.id, subscription_plan_id=subscription_plan.id, start_date=start_date, end_date=end_date
+    )
+
+    user.active_subscription = new_subscription
+
+    session.add(new_subscription)
+    await session.commit()
+
+    return new_subscription
+
+
+async def get_user_subscription_plan(session: AsyncSession, user: User) -> SubscriptionPlan:
+    await session.refresh(user, ["active_subscription"])
+
+    if user.active_subscription:
+        await session.refresh(user.active_subscription, ["subscription_plan"])
+        return user.active_subscription.subscription_plan
+
+    return None
