efb5d3deb87cc8d9cf15b48f8359a0ced71dc15e
Vadim Pchelintsev <vadim.1.95@mail.ru>
2024-09-09 21:17:25 +0300

    добавил новые роуты
    1. get_sub_plan_by_id
    2. buy_sub_plan_by_id
    3. get_me (доделал)

--- src/subscription/router.py
+++ src/subscription/router.py
@@ -11,8 +11,14 @@ from src.exceptions import CustomHTTPException
 from src.schemas import BaseResponse
 from src.auth.dependencies import get_current_user
 
-from .schemas import SubscriptionPlanResponse, SubscriptionPlanCreate, SubscriptionPlanRead
-from .service import create_subscription_plan, get_subscription_plan_list
+from .schemas import SubscriptionPlanResponse, SubscriptionPlanCreate, SubscriptionPlanRead, SubscriptionPurchase
+from .service import (
+    create_subscription_plan,
+    get_subscription_plan_list,
+    get_user_subscription_plan,
+    get_subscription_plan_by_id,
+    buy_subscription_plan,
+)
 
 
 subscription_router = APIRouter()
@@ -69,11 +75,56 @@ async def get_sub_plans(
     return BaseResponse(data=sub_plans)
 
 
-@subscription_router.get("/me")
-async def test_router(
+@subscription_router.get("/{plan_id}", response_model=SubscriptionPlanResponse)
+async def get_sub_plan_by_id(
+    plan_id: int,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    sub_plan = await get_subscription_plan_by_id(session, plan_id)
+
+    if not sub_plan:
+        raise CustomHTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Subscription plan not found.",
+        )
+
+    return SubscriptionPlanResponse(data=sub_plan)
+
+
+@subscription_router.post("/buy", response_model=SubscriptionPlanResponse)  # TODO: поменял с плана на подписку
+async def buy_sub_plan_by_id(
+    payment_data: SubscriptionPurchase,
+    current_user: User = Depends(get_current_user),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    try:
+        subscription_data = await buy_subscription_plan(session, current_user, payment_data)
+
+        if not subscription_data:
+            raise CustomHTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Failed to purchase subscription plan.",
+            )
+
+        return SubscriptionPlanResponse(data=subscription_data)
+    except ValueError as e:
+        raise CustomHTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e),
+        )
+
+
+@subscription_router.get("/me", response_model=SubscriptionPlanResponse)
+async def get_me(
     session: AsyncSession = Depends(db_helper.session_getter),
     current_user: User = Depends(get_current_user),
 ):
-    print("################################")
-    print(current_user)
-    print(current_user.email)
+    sub_plan = await get_user_subscription_plan(session, current_user)
+
+    if not sub_plan:
+        raise CustomHTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User subscription plan not found.",
+        )
+
+    return SubscriptionPlanResponse(data=sub_plan)  # TODO: поменял с плана на подписку
